bf01d78c41a6fd37546d70e4598f224c8544f0a0
ybisk <ybisk@yo-tech.com>
2016-01-13 13:23:31 -0800

    sparse dims

--- ModelA-S-T-RP.jl
+++ ModelA-S-T-RP.jl
@@ -3,8 +3,7 @@ using Requests
 
 # turns each row of data into a column of a one-hot sparse matrix
 # data should have a one based index for each attribute
-function sparsify(data)
-    colmax = maximum(data,1)
+function sparsify(data; colmax = maximum(data,1))
     offset = hcat(0,cumsum(colmax,2))
     I = Int[]
     J = Int[]
@@ -30,11 +29,11 @@ inputdim = size(x,1)
 outputdim = size(S,1)
 
 testdir = "BlockWorld/logos/Dev.STRP.data"
-data = readdlm(testdir);
-x_t  = sparsify(data[:,1:end-3]);
-S_t  = sparsify(data[:,end-2]);
-T_t  = sparsify(data[:,end-1]);
-RP_t = sparsify(data[:,end]);
+test_data = readdlm(testdir);
+x_t  = sparsify(test_data[:,1:end-3], colmax=maximum(data[:,1:end-3],1));
+S_t  = sparsify(test_data[:,end-2], colmax=maximum(data[:,end-2]));
+T_t  = sparsify(test_data[:,end-1], colmax=maximum(data[:,end-1]));
+RP_t = sparsify(test_data[:,end], colmax=maximum(data[:,end]));
 
 function train(f, data, loss)
     for (x,y) in data
