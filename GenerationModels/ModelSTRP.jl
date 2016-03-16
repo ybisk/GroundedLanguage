d66cfac344534f45ce35135177da2c192340815c
ozanarkancan <can.ozanarkan@gmail.com>
2016-03-17 01:37:35 +0200

    multi

--- GenerationModels/ModelSTRP.jl
+++ GenerationModels/ModelSTRP.jl
@@ -80,6 +80,24 @@ function testNRP(worldf, worlddata, loss, loc=false)
 	sumloss / numloss
 end
 
+function test(worldf, worlddata, loss, loc=false)
+	sumloss = numloss = 0
+	reset!(worldf)
+
+	wybatches = worlddata[1]
+	wposbatches = worlddata[2]
+
+	for i=1:length(wybatches)
+		x,y = wybatches[i]
+		_,pos = wposbatches[i]
+		ypred = forw(worldf, x, pos)
+		sumloss += loss(ypred, y)*size(y,2)
+		numloss += size(y,2)
+		reset!(worldf)
+	end
+	sumloss / numloss
+end
+
 flat(A) = mapreduce(x->isa(x,Array)? flat(x): x, vcat, [], A)
 
 function predictS(worldf, worlddata)
@@ -112,7 +130,7 @@ function predictNRP(worldf, worlddata)
 		x,y = wybatches[i]
 		_,pos = wposbatches[i]
 		ypred = forw(worldf, x, pos)
-		push!(ypreds, indmax(to_host(ypred)[:,1]))
+		push!(ypreds, to_host(ypred)[:,1])
 		reset!(worldf)
 	end
 	return ypreds
@@ -200,6 +218,43 @@ function get_worldsNRP(rawdata, targs; batchsize=100, predtype = "id", ftype=Flo
 	return (minibatch(worlds, y, batchsize), minibatch(worlds, targpos, batchsize))
 end
 
+function get_worldsMulti(rawdata, targs; batchsize=100, predtype = "id", ftype=Float32)
+	worlds = zeros(ftype, 60, size(rawdata, 1))
+
+	ydim = 190
+	y = zeros(ftype, ydim, size(rawdata, 1))
+	targpos = zeros(ftype, 3, size(rawdata, 1))
+
+	idsaresorted = true
+	nblocks=20
+
+	for i=1:size(rawdata, 1)
+		data = rawdata[i, :]
+
+		source = round(Int, data[1, 121])
+		worlds[:, i] = data[1, 1:60]'
+		strt = (source-1)*3 + 61
+		targpos[:, i] = targs[i]
+
+		_t1 = round(Int, data[1, 122])
+		_t2 = round(Int, data[1, 123])
+
+		t1 = min(_t1, _t2)
+		t2 = max(_t1, _t2)
+
+		(t1 != _t1 || t2 != _t2) && (idsaresorted = false)
+
+		indx = (t1-1)*nblocks - ((t1-1)*t1/2) + (t2 - t1)
+		indx = round(Int, indx)
+		y[indx, i] = 1.0
+
+	end
+
+	#!idsaresorted && (println("IDs are not sorted!!!"))
+	return (minibatch(worlds, y, batchsize), minibatch(worlds, targpos, batchsize))
+end
+
+
 function main(args)
 	s = ArgParseSettings()
 	s.exc_handler=ArgParse.debug_handler
@@ -208,7 +263,10 @@ function main(args)
 		("--modelABS"; default= "ModelAB-S.jld"; help="A+B => S")
 		("--modelABT"; default= "ModelAB-T.jld"; help="A+B => T")
 		("--modelATNR"; default= "ModelAB-NR.jld"; help="A+T => N+R")
+		("--modelMulti"; default= "Multi.jld"; help="A+T => R1+R2")
 		("--writefile"; action = :store_true; help="produces inputfile.predicted")
+		("--probs"; action = :store_true; help="give some predictions")
+		("--multi"; action = :store_true; help="multi")
 	end
 
 	isa(args, AbstractString) && (args=split(args))
@@ -227,27 +285,86 @@ function main(args)
 
 	preds = predictS(net1, worlddata)
 	rawdata[:,121] = preds
-
+	
 	worlddata = get_worldsT(rawdata, batchsize=1, oldstyle=false)
 
 	testerr = testT(net2, worlddata, quadloss)
 	println("Error of predicting the target loc (in terms of average block sizes): $(sqrt(2*testerr)/0.1254)")
-
 	preds = predictT(net2, worlddata)
-	worlddata = get_worldsNRP(rawdata, preds, batchsize=1, oldstyle=false)
-	
-	testerr = testNRP(net3, worlddata, zeroone)
-	println("Accuracy of predicting the N+RP: $(1-testerr)")
 
-	preds = predictNRP(net3, worlddata)
-	for i=1:length(preds)
-		rp, t = ind2sub((9, 20), preds[i])
-		rawdata[i, 122] = round(Int, t)
-		rawdata[i, 123] = round(Int, rp)
+	if o[:multi]
+		net4 = load(o[:modelMulti], "net")
+		testMulti = testNRP
+		predictMulti = predictNRP
+		worlddata = get_worldsMulti(rawdata, preds, batchsize=1)
+		testerr = testMulti(net4, worlddata, zeroone)
+		println("Accuracy of predicting the R1+R2: $(1-testerr)")
+		
+		mapping = Dict()
+		indx = 1
+		for i=1:19
+			for j=(i+1):20
+				mapping[indx] = (i,j)
+				indx += 1
+			end
+		end
+
+		preds = predictNRP(net4, worlddata)
+		for i=1:length(preds)
+			r1, r2 = mapping[round(Int, indmax(preds[i]))]
+			rawdata[i, 122] = r1
+			rawdata[i, 123] = r2
+		end
+
+		if o[:probs]
+			println("\n**** Examples ****")
+
+			for i=1:length(preds)
+				println("")
+				println(preds[i])
+				l = collect(enumerate(preds[i]))
+				sort!(l, by=(x -> x[2]), rev=true)
+				
+				println("Probs: 1: $(l[1][2]), 2: $(l[2][2])")
+				println("(R1, R2): 1: $(mapping[l[1][1]]), 2: $(mapping[l[2][1]])")
+
+				println("")
+			end
+		end
+
+
+	else	
+		worlddata = get_worldsNRP(rawdata, preds, batchsize=1, oldstyle=false)
+	
+		testerr = testNRP(net3, worlddata, zeroone)
+		println("Accuracy of predicting the N+RP: $(1-testerr)")
+
+		preds = predictNRP(net3, worlddata)
+		for i=1:length(preds)
+			rp, t = ind2sub((9, 20), indmax(preds[i]))
+			rawdata[i, 122] = round(Int, t)
+			rawdata[i, 123] = round(Int, rp)
+		end
+		if o[:probs]
+			println("\n**** Examples ****")
+
+			for i=1:length(preds)
+				println("")
+				println(preds[i])
+				l = collect(enumerate(preds[i]))
+				sort!(l, by=(x -> x[2]), rev=true)
+
+				println("Probs: 1: $(l[1][2]), 2: $(l[2][2])")
+				println("(Direction, Block): 1: $(ind2sub((9,20), l[1][1])), 2: $(ind2sub((9,20), l[2][1]))")
+
+				println("")
+			end
+		end
+
 	end
 
-
 	o[:writefile] && writedlm("$(o[:file]).predicted", rawdata, ' ')
-	
+
+		
 end
 !isinteractive() && main(ARGS)
