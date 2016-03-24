2c6a1d861d23bf62ee7fd28ee24e4fcef45840dd
ybisk <ybisk@yo-tech.com>
2016-03-24 11:49:50 -0400

    New joint JSON creation code which has a format easier for Java to parse

--- BlockWorld/DataCreation/DownloadMNISTdata.py
+++ BlockWorld/DataCreation/DownloadMNISTdata.py
@@ -12,6 +12,61 @@ directory = "trainset"
 #hit.json has text linked to action
 #annotated actions are in the hit.json
 
+
+class transObj():
+  def __init__(self, tran = [0,1], typ = "A0"):
+    self.type = typ
+    self.start = tran[0]
+    self.finish = tran[1]
+    self.users = []
+    self.notes = []
+
+  def add(self, u, n):
+    self.users.append(u)
+    self.notes.append(n)
+
+  def __eq__(self, other):
+    return self.start == other.start and self.finish == other.finish and self.type == other.type
+
+  def jsonable(self):
+    return self.__dict__
+
+class Task:
+  def __init__(self, name):
+    self.filename = name
+    self.side_length = 0.1524
+    self.shape_params = ["blue", "green", "cyan", "magenta", "yellow", "red"]
+
+  def add_states(self, given):
+    """
+    Convert to 3D array
+    """
+    self.states = []
+    self.images = []
+    for state in given:
+      current = []
+      for position in state["block_state"]:
+        current.append([position["position"]["x"], position["position"]["y"], position["position"]["z"]])
+      self.states.append(current)
+      self.images.append(state["screencapid"])
+
+  def add_decoration(self, decor):
+    self.decoration = decor
+
+  def add_notes(self, notes):
+    self.notes = notes
+
+  def jsonable(self):
+    return self.__dict__
+
+# http://stackoverflow.com/questions/5160077/encoding-nested-python-object-in-json
+def ComplexHandler(Obj):
+  if hasattr(Obj, 'jsonable'):
+    return Obj.jsonable()
+  else:
+    raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(Obj), repr(Obj))
+
+
 def convert(query, states):
   for i in range(len(states)):
     state = states[i]
@@ -30,13 +85,13 @@ if not os.path.exists(directory):
 
 j = json.load(gzip.open(directory + ".json.gz"))
 images = []
-JSON = {}
+JSON = []
 for file in j:
   ## Read information about the Simple state
   states = j[file]["simple"][0]["state"]["block_states"]
   for state in states:
     del state["created"]
-  notes = {}
+  notes = []
   for job in j[file]["simple"]:
     # Extract users who did the task correctly
     valid = []
@@ -49,10 +104,12 @@ for file in j:
     for user in job["hit"]["notes"]:
       if user in valid:
         for i in range(len(job["hit"]["notes"][user])):
-          if str(transitions[i]) not in notes:
-            notes[str(transitions[i])] = []
+          trans = transObj(transitions[i], "A0")
+          if trans not in notes:
+            notes.append(trans)
+          idx = notes.index(trans)
           for note in job["hit"]["notes"][user][str(i)]:
-            notes[str(transitions[i])].append({"user": user, "note": note})
+            notes[idx].add(user, note)
 
   ## Find the partial transitions to add to notes
   for job in j[file]["partial"]:
@@ -71,10 +128,12 @@ for file in j:
     for user in job["hit"]["notes"]:
       if user in valid:
         for i in range(len(job["hit"]["notes"][user])):
-          if str(transitions[i]) not in notes:
-            notes[str(transitions[i])] = []
+          trans = transObj(transitions[i], "A1")
+          if trans not in notes:
+            notes.append(trans)
+          idx = notes.index(trans)
           for note in job["hit"]["notes"][user][str(i)]:
-            notes[str(transitions[i])].append({"user": user, "note": note})
+            notes[idx].add(user, note)
 
   ## Full (jump from first to last)
   for job in j[file]["full"]:
@@ -87,31 +146,37 @@ for file in j:
     for user in job["hit"]["notes"]:
       if user in valid:
         for i in range(len(job["hit"]["notes"][user])):
-          if transition not in notes:
-            notes[transition] = []
+          trans = transObj(transitions[i], "A2")
+          if trans not in notes:
+            notes.append(trans)
+          idx = notes.index(trans)
           for note in job["hit"]["notes"][user][str(i)]:
-            notes[transition].append({"user": user, "note": note})
+            notes[idx].add(user, note)
 
-  JSON[file] = {}
-  JSON[file]["states"] = states
-  JSON[file]["annotations"] = notes
-  JSON[file]["decoration"] = j[file]["simple"][0]["state"]["block_meta"]["decoration"]
-  JSON[file]["block_properties"] = j[file]["simple"][0]["state"]["block_meta"]["blocks"][0]
+  task = Task(file)
+  task.add_states(states)
+  task.add_notes(notes)
+  task.add_decoration(j[file]["simple"][0]["state"]["block_meta"]["decoration"])
+  #task.block_properties = j[file]["simple"][0]["state"]["block_meta"]["blocks"][0]
+  JSON.append(task)
 
 # Get Images
 for file in JSON:
-  print "downloading images for ", file
-  for idx in range(len(JSON[file]["states"])):
+  print "downloading images for ", file.filename
+  for idx in range(len(file.states)):
     # Get image and create normal name
-    screencapid = JSON[file]["states"][idx]["screencapid"]
+    screencapid = file.images[idx]
     cmd = "https://cwc-isi.org/api/screencap/%s" % (screencapid)
     r = requests.get(cmd)
-    imagename = "%s_%s.png" % (file, str(idx) if idx > 9 else ('0' + str(idx)))
+    imagename = "%s_%s.png" % (file.filename, str(idx) if idx > 9 else ('0' + str(idx)))
     print imagename
     fh = open("%s/%s" % (directory, imagename), "wb")
     fh.write(r.text.split(",",1)[1].decode('base64'))
     fh.close()
-    del JSON[file]["states"][idx]["screencapid"]
-    JSON[file]["states"][idx]["image"] = imagename
+    file.images[idx] = imagename
 
-json.dump(JSON, gzip.open(directory + "/" + directory + ".json.gz", 'w'))
+
+out = gzip.open(directory + "/" + directory + ".json.gz", 'w')
+for file in JSON:
+  out.write(json.dumps(file, default=ComplexHandler)+ "\n")
+out.close()
