dbd33ec797d5ad0d15a4414e6beadb51b1fcb235
ybisk <ybisk@yo-tech.com>
2016-03-30 14:59:10 -0700

    Dead models

--- ActionUnderstanding/RNN-SRxyz.jl
+++
@@ -1,7 +0,0 @@
-# Recurrent neural network which predicts Source, Target, and uses them to predict XYZ coordinates
-# Input File: JSONReader/data/2016-NAACL/STxyz/*.mat
-
-using Knet
-using ArgParse
-using JLD
-using CUDArt
